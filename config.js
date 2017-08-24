ea0f415a2b9b3655087abf7741342e17b6e411a1
Nilesh Patel <nileshp88@gmail.com>
2017-08-24 01:06:02 -0400

    commit config for production

--- config.js
+++ config.js
@@ -5,18 +5,14 @@ config.adminName = "Admin";
 config.adminUsername = "admin";
 config.defaultAdminPassword = "admin";
 config.cookieSecret = 'aopr8bsr28ga786noytchons';
-
 config.failsBeforeLockout = 5;
 config.lockoutLength = 30; // in seconds
-config.nukeOnRestart = true; // debugging purposes
+config.nukeOnRestart = false; // debugging purposes
 config.usernameRegex = /^[\d\w]{4,}$/; // succeeds if match, allows letters, numbers, and underscores
 config.nameRegex = /^[a-zA-Z'-\s]*$/; // succeeds if match, allows letters and spaces
-
 config.labMonitorPassphrase = "labMonitor2015";
 config.execPassphrase = "execsAreExecs";
 config.adminPassphrase = "adminzRule";
-
 config.internalPort = 8080;
-config.externalPort = 8181;
-
+config.externalPort = 80;
 module.exports = config;
