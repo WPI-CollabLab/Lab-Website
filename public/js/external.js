0bac1d49519c71ee7dc7ab55c37ad1f0c9195947
Peter Mitrano <mitranopeter@gmail.com>
2017-09-04 14:37:10 -0400

    remove kicking logic for external page
    use mixin to improve the jade

--- public/js/external.js
+++ public/js/external.js
@@ -1,13 +1,6 @@
 var labStatus = null;
 getStatus();
-var kicking = kicking || false;
 setInterval(getStatus, 5000);
-
-$('#kickModal').on('hidden.bs.modal', function(){
-  $('#idNumber').val('');
-  getStatus();
-});
-
 
 function getStatus(){
   getData('/lab/status', function(response){
@@ -29,24 +22,4 @@ function updatePage(newStatus){
 
 function login(){
   window.location.replace('/manage');
-}
-
-function kick(username){
-  $("#kickName").html(labStatus.members[username]);
-  $("#kickModal").modal();
-  $('#idNumber').val(username);
-}
-
-function kickUser(){
-  var idNumber = $('#idNumber').val();
-  var data = {'idNumber':idNumber};
-  postData('/lab/kick',JSON.stringify(data), function(statusCode){
-    console.log(statusCode);
-    switch(statusCode){
-      case 0:
-        $.notify("Kick Successful!", {'className':'success'});
-        $('#kickModal').modal('hide');
-      break;
-    }
-  });
 }
